64c65cffdfc6d747e23114562e4787ba0f0f65cb
Jay Packer <jay@jaypacker.com>
2018-03-29 09:34:28 -0500

    Data persistence using UserDefaults.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    //Holds list of todo items
     var itemArray = ["Pet Bruce", "Love Bruce", "Ask Bruce to stop biting me"]
+    
+    //User Defaults creates a plist file that holds key/value pairs. Can be used for small amounts of storage.
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Read array from User Defaults
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
     
     //MARK: - Tableview Datasource Methods
@@ -63,8 +72,12 @@ class TodoListViewController: UITableViewController {
         
         let action  = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //When user clicks the Add Item button on our UIAlert
-            print("Success.")
+
             self.itemArray.append(textField.text!)
+
+            //Save our array of todo items in user defaults. The key is used to retrieve item.
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
